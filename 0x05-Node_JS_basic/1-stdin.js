ad3e335e56e6844493f0a61b486e821370049109
GeraldMajoni <majonigerald@gmail.com>
2023-11-22 01:13:10 +0200

    Add files via upload

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+/**
+ * Using Process stdin
+ */
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const INPUT = process.stdin.read();
+
+  if (INPUT) {
+    process.stdout.write(`Your name is: ${INPUT}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
